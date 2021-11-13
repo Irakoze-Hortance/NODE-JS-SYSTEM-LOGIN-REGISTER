57dea77e349e2f08098c6e9360abfe64154b0546
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-13 16:36:48 +0200

    modified user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,29 @@
-const db=require('../config/database')
 const mongoose=require('mongoose')
-var userSchema=new mongoose.Schema({
-    name:String,
-    password:String,
-    email:String,
-});
+const Joi=require('joi')
+Joi.objectId=require('joi-objectid')(Joi)
 
-var userModel=mongoose.model('users',userSchema);
-module.exports=mongoose.model("Users",userModel);
+
+
+const User =mongoose.model("User",
+new mongoose.Schema({
+    fullName:String,
+    email:String,
+    password:String,
+    userRole:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"roles",
+    },
+})
+);
+
+const validateUser=(user)=>{
+    const JoiSchema=Joi.object({
+        fullName:Joi.string().min(3).max(40).required(),
+        email:Joi.string().required(),
+        password:Joi.string().required().min(4).max(20),
+        userRole:Joi.objectId().required(),
+    });
+    return joiSchema.validate(user);
+}
+module.exports.User=User;
+module.exports.validateUser=validateUser;
