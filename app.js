35d4234c00ad488a4507857b7027dd6e2c9df1a4
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-13 21:53:48 +0200

    modified index page

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var mongoose= require('mongoose')
+const cors=require("cors")
 var passport = require('passport');
+const userRoutes=require('./routes/userRoutes')
 var bodyParser = require('body-parser');
-// var LocalStrategy = require('passport-local-strategy');
-var passportLocalMongoose =require('passport-local-mongoose')
-var User=require("./models/userModel")
 var app = express();
+require("./config/database")
 
+app.use(cors)
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('express-session')({
@@ -14,76 +14,15 @@ app.use(require('express-session')({
     resave:false,
     saveUninitialized:false
 }));
-app.use(passport.initialize());
-app.use(passport.session());
-// passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
+app.use(express.json());
 
-app.get("/",function(req,res){
-    res.render('register', {
-        title:"Registration page",
-        name:'',
-        email:'',
-        password:'',
-    })
+app.get("/", (req, res) => {
+  res.send("Welcome");
 });
 
-app.get("/home",isLoggedIn,function(req,res){
-    res.render('home');
-})
-
-app.get("/register",function(req,res){
-    res.render('register',{
-        title:"Registration page",
-        name:'',
-        email:'',
-        password:'',
-    })
-});
-
-app.post("/register",function(req,res){
-    var email = req.body.email;
-    var password = req.body.password;
-    User.register(new User({email:email}),
-    password,function(err,user){
-        if(err){
-            console.log(err);
-            return res.render("register");
-        }
-        passport.authenticate("local")(
-            res,res,function(){
-                req.flash("success","You have logged in")
-                res.render("home");
-            });
-    });
-});
-
-app.get("/login",function(req,res){
-    res.render('login',{
-        title:"Login",
-        email:'',
-        password:''
-    })
-})
-
-app.post("/login",passport.authenticate("local",{
-    successRedirect:"/home",
-    failureRedirect:"/login"
-}),function(req,res){
-})
-
-app.get("/logout",function(req,res){
-    req.logOut();
-    res.redirect("/");
-});
-
-function isLoggedIn(req,res,next) {
-    if(req.isAuthenticated()) return next();
-    res.redirect("/login");
-}
-
+const baseUrlRoute="/api";
+app.use(`${baseUrlRoute}users/`,userRoutes)
 var port = process.env.PORT ||3000;
 app.listen(port,function(){
     console.log("Server has started");
