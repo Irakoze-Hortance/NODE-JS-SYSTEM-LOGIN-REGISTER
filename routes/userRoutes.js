19a3662937cedeefa117fd63855ad71a30927fc2
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-13 20:08:15 +0200

    added user routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+const {authJwt}=require('../middlewares')
+const express = require('express')
+const router=express.Router()
+const userController=require('../controllers/userController')
+
+router.get("/all",userController.allAccess)
+router.get("/user",[authJwt.verifyToken,authJwt.isAdmin],userController.userBoard)
+router.get("/admin",[authJwt.verifyToken,authJwt.isAdmin],userController.adminBoard)
+
+module.exports=router
