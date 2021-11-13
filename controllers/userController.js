c45323046a8f3ee506a690e8ac4f3da75039cb67
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-13 17:30:09 +0200

    user controller

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+allAccess=(req,res)  =>{
+  res.status(200).send("Public content")
+
+}
+
+userBoard=(req,res)=>{
+  res.status(200).send("User content")
+}
+
+adminBoard=(req,res)=>{
+  res.status(200).send("Admin content")
+}
+
+module.exports={
+  allAccess,
+  userBoard,
+  adminBoard
+}
