bb2d87076d9600c1ece092fa2227f74202ac89dc
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-14 18:41:11 +0200

    check duplicate email

--- middlewares/verifySignUp.js
+++ middlewares/verifySignUp.js
@@ -2,3 +2,16 @@ const db=require('../models')
 const {User}=db.User
 const Role=db.role
 const mongoose=require('mongoose')
+
+const checkDuplicateEmail=(req,res,next) => {
+    User.findOne({ 
+        email: req.body.email,
+    }).exec((err, user) => {
+        if(err) return res.status(500).send({ message:err.message});
+
+        if(user){
+            return res.status(200).send({ message:"Failed! Email already in use "})
+        }
+        next();
+    })
+}
