da45d91bf71a689c060678838b26b7dbbed52f1c
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-18 09:11:12 +0200

    user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,29 +1,17 @@
-const mongoose=require('mongoose')
-const Joi=require('joi')
-Joi.objectId=require('joi-objectid')(Joi)
+const mongoose=require("mongoose") 
 
+const User=mongoose.model(
+    "User",
+    new mongoose.Schema({
+        username:String,
+        email:String,
+        password:String,
+        roles:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"Role"
+            }
+        ]
+    }));
 
-
-const User =mongoose.model("User",
-new mongoose.Schema({
-    fullName:String,
-    email:String,
-    password:String,
-    userRole:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"roles",
-    },
-})
-);
-
-const validateUser=(user)=>{
-    const joiSchema=Joi.object({
-        fullName:Joi.string().min(3).max(40).required(),
-        email:Joi.string().required(),
-        password:Joi.string().required().min(4).max(20),
-        userRole:Joi.objectId().required(),
-    });
-    return joiSchema.validate(user);
-}
-module.exports.User=User;
-module.exports.validateUser=validateUser;
+module.exports=User;    
