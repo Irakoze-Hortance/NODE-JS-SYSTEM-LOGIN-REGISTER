2797a9633290fda55021cedc6bdf6081c6fd5e70
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-18 10:45:07 +0200

    user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,15 @@ const express = require('express')
 const router=express.Router()
 const userController=require('../controllers/userController')
 
-router.get("/all",userController.allAccess)
-router.get("/user",[authJwt.verifyToken,authJwt.isAdmin],userController.userBoard)
-router.get("/admin",[authJwt.verifyToken,authJwt.isAdmin],userController.adminBoard)
-router.post("/",userController.allAccess)
-
-module.exports=router
+module.exports=function(app){
+    app.use(function(req,res,next){
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token,Origin,Content-Type,Accept"
+        );
+        next();
+    })
+    app.get("/api/test/all",userController.allAccess)
+    app.get("/api/test/user",[authJwt.verifyToken],userController.userBoard);
+    app.get("/api/test/mod",[authJwt.verifyToken,authJwt.isAdmin],userController.adminBoard);
+}
