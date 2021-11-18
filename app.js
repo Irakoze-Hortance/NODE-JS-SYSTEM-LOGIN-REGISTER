0da1b74e3891c0e464a88267f387264dc24460e4
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-18 18:55:27 +0200

    imports

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-var express = require('express');
-const cors=require("cors")
-var passport = require('passport');
-const userRoutes=require('./routes/userRoutes')
-var bodyParser = require('body-parser');
-var app = express();
-require("./config/database")
-
-app.use(cors)
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(require('express-session')({
-    resave:false,
-    saveUninitialized:false
-}));
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Welcome");
-});
-
-const baseUrlRoute="/api";
-app.use(`${baseUrlRoute}users/`,userRoutes)
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+const express=require('express')
+const bodyParser=require('body-parser')
+const cors=require('cors')
+const dbConfig=require('./config/database')
