0c822b8d15b90a8c4824106c567921568a4886f8
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-18 09:49:50 +0200

    verify sign up

--- middlewares/verifySignUp.js
+++ middlewares/verifySignUp.js
@@ -1,43 +1,51 @@
 const db=require('../models')
 const {User}=db.user
-const Role=db.role
+const ROLES=db.ROLES
 const mongoose=require('mongoose')
 
-const checkDuplicateEmail=(req,res,next) => {
+const checkDuplicateEmailOrUsername=(req,res,next) => {
     User.findOne({ 
-        email: req.body.email,
+        username: req.body.username,
     }).exec((err, user) => {
         if(err) return res.status(500).send({ message:err.message});
 
         if(user){
-            return res.status(200).send({ message:"Failed! Email already in use "})
+            return res.status(200).send({ message:"Failed! Username already in use "})
         }
-        next();
+        User.findOne({
+            email:req.body.email,
+        }).exec((err,user)=>{
+            if(err){
+                res.status(500).send({ message:err});
+                return;
+            }
+            if(user){
+                res.status(400).send({message:"Email already in use!"})
+                return;
+            }
+            next();
+        })
+        
     })
 }
 
-checkRolesExisted=async(req,res,next) => {
-    const isValid=await mongoose.Types._ObjectId.isValid(req.body.userRole);
-    if(!isValid) return res.send({ message:"Id must be a valid mongoose object id"})
-
-    if(req.body.userRole){
-        await Role.findById(req.body.userRole)
-        .then((role)=>{
-            if(!role){
-                return res.status(404).send({ message:`Failed! Role ${req.body.userRole} does not exit`})
-            }else{
-                next();
+checkRolesExisted=(req,res,next) => {
+    if(req.body.roles){
+        for(let i=0;i<req.body.roles.length;i++){
+            if(!ROLES.includes(req.body.roles[i])){
+                res.status(404).send({
+                    message:`Failed Role ${req.body.roles[i]} does not exist`
+                });
+                return;
             }
-
-        })
-        .catch((err)=>{
-            return res.status(400).send({ message:err.message})
-        })
+        }
     }
+    next();
+    
 }
 
 const verifySignUp={
-    checkDuplicateEmail,
+    checkDuplicateEmailOrUsername,
     checkRolesExisted,
 }
 module.exports=verifySignUp
