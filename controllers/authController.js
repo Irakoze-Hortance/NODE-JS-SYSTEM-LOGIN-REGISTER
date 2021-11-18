9c4a26a48112ed8a24aaac40fbfb033f27701e4a
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-18 09:28:26 +0200

    user signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,27 +1,59 @@
 const config=require('../config/authConfig')
 const db=require('../models')
-const {User,validateUser}=db.user;
+const User=db.user;
+const Role=db.role;
+
 var jwt = require('jsonwebtoken')
 var bcrypt = require('bcryptjs')
 
-exports.signup=async(req,res)=>{
-    const {error}=validateUser(req.body);
-    if(error) return res.status(400).send(error);
+exports.signup=(req,res)=>{
 
     const user=new User({
-        fullName:req.body.fullName,
+        username:req.body.username,
         email:req.body.email,
         password:bcrypt.hashSync(req.body.password,8),
-        userRole:req.body.userRole,
     });
-    await user
-    .save()
-    .then(() => {
-        return res.status(201).send({message:"User registered  successfully"});
-    })
-    .catch((err) => {
-        return res.status(500).send({message:err})
-    })
+     user.save((err,user)=>{
+         if(err){
+             res.status(500).send({message:err})
+         }
+     })
+    
+    if(req.body.roles){
+        Role.find({
+            name:{$in:req.body.roles}
+        },(err,roles) =>{
+            if(err){
+                res.status(500).send({message:err});
+                return;
+            }
+            user.roles=roles.map(role=>role._id);
+            user.save(err=>{
+                if(err){
+                    res.status(500).send({message:err});
+                    return;
+                }
+                res.send({message:"User registered successfully"});
+            })
+        }
+        )
+    }else{
+
+        Role.findOne({name:"user"},(err,role)=>{
+            if(err){
+                res.status(500).send({message:err});
+                return;
+            }
+            user.roles=[role._id];
+            user.save(err=>{
+                if(err){
+                    res.status(500).send({message:err});
+                    return;
+                }
+                res.send({message:"User registered successfully"});
+            })
+        })
+    }
     
 };
 
