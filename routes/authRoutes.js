632a31ac03f9e1732f5d4d151fb0e8721db8731f
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-18 10:53:22 +0200

    auth routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,15 @@
+const {verifySignUp}=require("../middlewares");
+const controller=require('../controllers/authController')
+
+module.exports=function(app){
+    app.use(function(req,res,next){
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-acess-token,Origin,Content-Type,Accept"
+        );
+        next();
+    })
+
+    app.post('/api/auth/signup',[verifySignUp.checkDuplicateEmailOrUsername,verifySignUp.checkRolesExisted],controller.signup);
+    app.post('api/auth/signin',controller.signin)
+}
