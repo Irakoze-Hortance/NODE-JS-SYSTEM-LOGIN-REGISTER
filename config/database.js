218d757b6e6317ed6fd4aa6bbcc5194ff647b283
Irakoze Hortance <hortanceirakoze@gmail.com>
2021-11-19 07:45:19 +0200

    initial commit

--- config/database.js
+++ config/database.js
@@ -1,13 +1,5 @@
-const db = require('../models');
-const uri = "mongodb://localhost:27017/docs";
-db.mongoose.connect(uri,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-})
-
-.then(() =>{
-    console.log("Connection succefully established")
-})
-.catch(err =>{
-    console.error("Error connecting",err)
-})
+module.exports = {
+    HOST: "localhost",
+    PORT: 27017,
+    DB: "archive"
+  };
