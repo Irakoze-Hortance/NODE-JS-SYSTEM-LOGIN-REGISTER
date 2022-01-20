3efcdcd8af2f45065f9bbb0df3af24bbdd1cc6ce
Irakoze-Hortance <hortanceirakoze@gmail.com>
2022-01-20 08:09:14 +0200

    feat:token parsing

--- jwt-auth/src/common/auth-verify.js
+++ jwt-auth/src/common/auth-verify.js
@@ -1,2 +1,10 @@
 import  React,{Component}  from 'react'
 import {withRouter} from 'react-router-dom'
+
+const parseJwt=(token)=>{
+    try{
+        return JSON.parse(atob(token.split('.')[1]));
+    }catch(e){
+        return null;
+    }
+};
