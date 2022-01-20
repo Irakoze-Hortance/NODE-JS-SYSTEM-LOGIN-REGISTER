453e2358060121ebdf801180d5f8249250850887
Irakoze-Hortance <hortanceirakoze@gmail.com>
2022-01-20 11:09:12 +0200

    added required

--- jwt-auth/src/components/register.component.js
+++ jwt-auth/src/components/register.component.js
@@ -3,3 +3,15 @@ import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
 import {isEmail} from "validator";
+import AuthService from "../services/auth.service";
+
+const required=value=>{
+    if(!value){
+        return(
+            <div classNAme="alert alert-danger">
+                This  is a required field
+            </div>
+        );
+    }
+};
+
