38fafec1934af3c34bfe50cec69eece559ebfdb6
Irakoze-Hortance <hortanceirakoze@gmail.com>
2022-01-20 11:01:08 +0200

    variables to be posted

--- jwt-auth/src/services/auth.service.js
+++ jwt-auth/src/services/auth.service.js
@@ -3,5 +3,11 @@ import axios from 'axios';
 const API_URL="Http://localhost:8080/api/auth/";
 
 class  AuthService{
-    
+    login(username,password){
+        return axios
+            .post(API_URL,+"signin",{
+                username,
+                passww
+            })
+    }
 }
