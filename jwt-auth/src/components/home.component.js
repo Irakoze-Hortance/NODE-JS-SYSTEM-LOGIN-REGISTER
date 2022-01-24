d4707e438821f25134c0e44316c4b166a7230af3
Irakoze-Hortance <hortanceirakoze@gmail.com>
2022-01-24 11:24:48 +0200

    get public content success

--- jwt-auth/src/components/home.component.js
+++ jwt-auth/src/components/home.component.js
@@ -1,2 +1,22 @@
 import React,{Component} from "react";
 import userService from "../services/user.service";
+
+export default class Home extends Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            content:""
+        };
+    }
+
+    componentDidMount(){
+        userService.getPublicContent().then(
+            response=>{
+                this.setState({
+                    content:response.data
+                })
+            }
+        )
+    }
+}
