118d98e2615c81faaee2a0825a2552a18ab2272c
Irakoze-Hortance <hortanceirakoze@gmail.com>
2022-01-24 11:30:04 +0200

    feat:finished public homme component

--- jwt-auth/src/components/home.component.js
+++ jwt-auth/src/components/home.component.js
@@ -29,5 +29,13 @@ export default class Home extends Component{
             }
         );
     }
-    
+    render(){
+        return(
+            <div className="container">
+                <header className="jumbotron">
+                    <h3>{this.state.content}</h3>
+                </header>
+            </div>
+        )
+    }
 }
